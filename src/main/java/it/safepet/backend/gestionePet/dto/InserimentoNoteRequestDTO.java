36254c48902c1751e8febab82baca2bbdcdb9e2c
Darkolicchio <simonecimmino2004@gmail.com>
2025-11-24 16:15:31 +0100

    fix: correzione errori checkstyle

--- src/main/java/it/safepet/backend/gestionePet/dto/InserimentoNoteRequestDTO.java
+++ src/main/java/it/safepet/backend/gestionePet/dto/InserimentoNoteRequestDTO.java
@@ -1,7 +1,6 @@
 package it.safepet.backend.gestionePet.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class InserimentoNoteRequestDTO {
